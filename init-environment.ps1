303be5ee5d6a854fd1ece99c8c195c2af20f8ddc
GuilhermeOchoa <guilhermeochoa@gmail.com>
2025-03-19 10:22:10 -0300

    docker compose up

--- init-environment.ps1
+++ init-environment.ps1
@@ -17,5 +17,7 @@ docker run -d --name postgres -p 5432:5432 -e POSTGRES_DB=keycloak -e POSTGRES_U
 Write-Host "`nStarting Keycloak"
 docker run -d --name keycloak -p 8080:8080 -e KC_BOOTSTRAP_ADMIN_USERNAME=admin -e KC_BOOTSTRAP_ADMIN_PASSWORD=admin -e KC_DB=postgres -e KC_DB_URL_HOST=postgres -e KC_DB_URL_DATABASE=keycloak -e KC_DB_USERNAME=keycloak -e KC_DB_PASSWORD=password --network=springboot-react-keycloak-net quay.io/keycloak/keycloak:$KEYCLOAK_VERSION start-dev
 
+docker start $(docker ps -aq)
+
 Write-Host "`nEnvironment Up and Running"
 Write-Host "=========================="
